b5cd8a410fbede72236004597b80bc387b6e7fc9
Ruo Liu <ruoliu.dev@gmail.com>
2024-06-24 21:13:54 -0500

    feat: add checkbox to questions, allow selection of question

--- src/app/_question.tsx
+++ src/app/_question.tsx
@@ -1,40 +1,87 @@
 'use client';
 import React, {useState} from 'react';
-import {Box, Checkbox, FormControlLabel, IconButton, List, ListItem, ListItemText, Typography,} from '@mui/material';
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  Link,
+  List,
+  ListItem,
+  ListItemText,
+  Typography
+} from '@mui/material';
 import topics from '../../public/data/top150.json';
 import {Shuffle} from '@mui/icons-material';
 
+const QUESTION_SELECTION = 'questionSelection';
 const difficultyColors: { [key: string]: string } = {
   Easy: 'green',
   Medium: 'orange',
   Hard: 'red',
 };
-
+type Filter = {
+  [key: string]: boolean;
+};
+type QuestionSelection = {
+  [key: string]: boolean;
+};
+const difficulties = ['Easy', 'Medium', 'Hard'];
+const initFilter = {
+  Easy: true,
+  Medium: true,
+  Hard: true,
+}
+const isBrowser = typeof window !== 'undefined';
 const Question = () => {
-  type Filter = {
-    [key: string]: boolean;
-  };
-  const [filter, setFilter] = useState<Filter>({
-    Easy: true,
-    Medium: true,
-    Hard: true,
+  // yseStates and useEffects
+  const [filter, setFilter] = useState<Filter>(initFilter);
+  const [questionSelection, setQuestionSelection] = useState<QuestionSelection>(() => {
+    // TODO: implement local storage
+    // if (isBrowser) {
+    //   const savedSelection = localStorage.getItem(QUESTION_SELECTION);
+    //   if (savedSelection) return JSON.parse(savedSelection);
+    // }
+    // If no selection is found or not browser, initialize all questions as selected
+    const defaultSelection: { [key: string]: boolean } = {};
+    Object.entries(topics).forEach(([topic, questions]) => {
+        questions.forEach(([name, link, difficulty]) => {
+          defaultSelection[name] = true;
+        });
+      }
+    );
+    return defaultSelection;
   });
+  // useEffect(() => {
+  //   localStorage.setItem(QUESTION_SELECTION, JSON.stringify(questionSelection));
+  // }, [questionSelection]);
 
+  // custom functions
   const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFilter({...filter, [event.target.name]: event.target.checked});
   };
-
   const getRandomQuestion = (questions: string[][]) => {
     const filteredQuestions = questions.filter(
-      ([name, link, difficulty]) => filter[difficulty]
+      ([name, link, difficulty]) => filter[difficulty] && questionSelection[name]
     );
+    if (filteredQuestions.length === 0) {
+      return;
+    }
     const randomIndex = Math.floor(Math.random() * filteredQuestions.length);
     const [name, link, difficulty] = filteredQuestions[randomIndex];
+    // mark question as not selected
+    setQuestionSelection({...questionSelection, [name]: false,});
     window.open(link, '_blank');
   };
-
-  const difficulties = ['Easy', 'Medium', 'Hard'];
-
+  const handleSelectionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.name, event.target.checked)
+    setQuestionSelection(prevState => ({...prevState, [event.target.name]: event.target.checked,}))
+  }
+  const handleOnClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, name: string, link: string) => {
+    setQuestionSelection(prevState => ({...prevState, [name]: false,}))
+    window.open(link, '_blank');
+    event.preventDefault();
+  }
   return (
     <Box>
       <Box>
@@ -75,16 +122,28 @@ const Question = () => {
             {questions.map(
               ([name, link, difficulty], index) =>
                 filter[difficulty] && (
-                  <ListItem key={index} component="a" href={link} divider>
-                    <ListItemText
-                      primary={name}
-                      secondary={difficulty}
-                      secondaryTypographyProps={{
-                        style: {
-                          color: difficultyColors[difficulty] || 'black',
-                        },
-                      }}
+                  <ListItem
+                    key={index}
+                    divider
+                  >
+                    <Checkbox
+                      key={name}
+                      checked={questionSelection[name]}
+                      name={name}
+                      onChange={handleSelectionChange}
                     />
+                    <Link href={link} underline="none" target={'_blank'}
+                          onClick={(event) => handleOnClick(event, name, link)}>
+                      <ListItemText
+                        primary={name}
+                        secondary={difficulty}
+                        secondaryTypographyProps={{
+                          style: {
+                            color: difficultyColors[difficulty] || 'black',
+                          },
+                        }}
+                      />
+                    </Link>
                   </ListItem>
                 )
             )}
