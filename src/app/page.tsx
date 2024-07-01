97399d09d7efc8f551205ed710f5d77e08bcd820
Ruo Liu <ruoliu.dev@gmail.com>
2024-07-01 01:50:59 -0500

    feat: add dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
 'use client';
 import {Container} from '@mui/material';
 import Question from '@/app/components/question';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import React from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 export default function Home() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <Container>
-      <Question/>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Container>
+        <Question/>
+      </Container>
+    </ThemeProvider>
   );
 }
