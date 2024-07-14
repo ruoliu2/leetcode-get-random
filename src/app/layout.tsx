2189662d49d26ce9f2a9ffdb43bf582a28ef92d8
Ruo Liu <ruoliu.dev@gmail.com>
2024-07-13 22:52:34 -0500

    doc: doc fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import type {Metadata} from 'next';
-import {Inter} from 'next/font/google';
 import {Container} from '@mui/material';
 
-const inter = Inter({subsets: ['latin']});
-
 export const metadata: Metadata = {
-  title: 'leetcode-top-150',
-  description: 'random questiosn from leetcode top 150',
+  title: 'leetcode-get-random',
+  description: 'random questiosn from leetcode lists',
 };
 
 export default function RootLayout({
